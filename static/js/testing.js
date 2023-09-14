fb659d9739c87089eb7c4cc632c2f35dfb67a6f2
Jeff DuBois <jeffdubo@gmail.com>
2023-09-14 15:34:52 -0700

    Update testing.js

--- static/js/testing.js
+++ static/js/testing.js
@@ -3,7 +3,7 @@
 // DataSF portal
 // SF Hate Crimes main page: https://data.sfgov.org/Public-Safety/Police-Department-Investigated-Hate-Crimes/huqr-9p9x
 // API Docs: https://dev.socrata.com/foundry/data.sfgov.org/huqr-9p9x
-const url = "https://data.sfgov.org/resource/huqr-9p9x.json?$limit=3000";
+const url = "https://data.sfgov.org/resource/huqr-9p9x.json?$where=occurence_month < '2023-07-01T00:00:00.000'"
 
 // Fetch the JSON data and console log for review
 d3.json(url).then(function(data) {

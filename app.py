e20cad980eee3dbe713c87f65bf1adb0757eb59d
evacs <evcschmi@ucsc.edu>
2023-09-23 13:00:52 -0700

    successfuly began converting main_incidents table to JSON dict

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # Hate Crimes in the US
 # Flask App
 
-# Import dependencies
-from flask import Flask, render_template
-import sqlalchemy
+from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, inspect
-from flask import Flask, render_template
 
-connection_string = "postgresql://admin:fRFTp6MgD7AgfQYMYmyM5jaR8KAfKyXV@dpg-ck56k66ru70s738p5s4g-a.oregon-postgres.render.com:5432/us_hate_crimes"
+app = Flask(__name__)
 
-engine = create_engine(connection_string)
+# Create a SQLAlchemy database engine
+db_uri = 'postgresql://admin:fRFTp6MgD7AgfQYMYmyM5jaR8KAfKyXV@dpg-ck56k66ru70s738p5s4g-a.oregon-postgres.render.com:5432/us_hate_crimes'
+engine = create_engine(db_uri)
+
 
 
 # reflect an existing database into a new model
@@ -22,31 +22,54 @@ Base.prepare(autoload_with=engine)
 print(Base.classes.keys())
 session = Session(engine)
 
-# Create app
-app = Flask(__name__)
 
-# Define static routes
-@app.route('/')
+#Define static routes
+
+# Launches site
+@app.route('/') 
 def index():
     return render_template('index.html')
 
-@app.route('/api/app1')
-def app1():
-    return 'Application 1'
+# Turns main_incidents table into a JSON dictionary
+@app.route('/data')
+def get_data():
 
-@app.route('/api/app2')
-def app2():
-    return 'Application 2'
+    try:
 
-@app.route('/api/stateIncidents')
-def stateIncidents():
+        result = session.query(Base.classes.main_incidents).all()
+        # Convert the query result to a list of dictionaries              
+        dataToReturn = {}
 
-    incident_biases = session.query(bias,incident_biases_2,incedents).join(bias.bias_id==incidents_biases_2.bias_id).join(incidents, incidents_id=incidents_biases_2.incident_id).group_by(incidents.state_abbr).all()
-    data_dict_list = [row.as_dict() for row in incident_biases]
+        incident_ids = []
+        state_abbrs = []
 
-    # Use jsonify to pretty print the JSON response
-    return jsonify(incident_biases=data_dict_list), 200
+        for row in result:
+            incident_ids.append(row.__dict__["incident_id"])
+            state_abbrs.append(row.__dict__["state_abbr"])
+
+        dataToReturn["incident_id"] = incident_ids
+        dataToReturn["state_abbr"] = state_abbrs
+        # Print a success message
+        print("Table access successful")
+
+        return jsonify(dataToReturn)
+
+    except Exception as e:
+        # Handle and log any exceptions
+        print("Error accessing the table:", str(e))
+        return jsonify({"error": "Table access failed"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
+
+    
+# @app.route('/api/app1')
+# def app1():
+#     return 'Application 1'
+
+# @app.route('/api/app2')
+# def app2():
+#     return 'Application 2'
+
+
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,6 @@
 // Hate Crimes in the US Code
+
+d3.json("http://127.0.0.1:5000/data")
+  .then(function (data) { 
+    console.log(data); 
+    })

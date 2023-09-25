082df9dedb34287d5291288f6d2cb8e9eb2daa7e
mattcalvrt <matthew.a.calvert@gmail.com>
2023-09-25 08:56:05 -0700

    plotly attempt

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, render_template
 from sqlalchemy import create_engine, text
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 
 app = Flask(__name__)
 
@@ -61,14 +62,45 @@ def get_data():
         # Handle and log any exceptions
         print("Error accessing the table:", str(e))
         return jsonify({"error": "Table access failed"}), 500
+    
+@app.route('/matt')
+def get_data_matt():
+
+    try:
+        
+        # Save a reference to the main_incidents table as `main_incidents`
+        main_incidents = Base.classes.main_incidents
+
+        # Design a query that lists the count of hate crimes by state and bias description
+        state_data = session.query(
+            main_incidents.state_name,
+            main_incidents.bias_desc,
+            main_incidents.data_year,
+            func.count(main_incidents.incident_id).label("count")
+        ).group_by(
+            main_incidents.state_name,
+            main_incidents.bias_desc,
+            main_incidents.data_year
+        ).order_by(main_incidents.state_name.asc()).all()
+
+        # Convert the query result to a list of dictionaries              
+        state_data_list = [{"data_year": row.data_year, "state_name": row.state_name, "bias_desc": row.bias_desc, "count": row.count} for row in state_data]
+        
+        # Assign the metadata list to the "metadata" key in the data dictionary
+        dataToReturn = {"state_data": state_data_list}
+
+        # Print a success message
+        print("Table access successful")
+
+        return jsonify(dataToReturn)
+
+    except Exception as e:
+        # Handle and log any exceptions
+        print("Error accessing the table:", str(e))
+        return jsonify({"error": "Table access failed"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
-# @app.route('/api/app1')
-# def app1():
-#     return 'Application 1'
 
 # @app.route('/api/app2')
 # def app2():
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,67 @@
 // Hate Crimes in the US Code
 
 d3.json("http://127.0.0.1:5000/data")
-  .then(function (data) { 
+  .then(function (data) {
     console.log(data); 
     })
+
+  d3.json("http://127.0.0.1:5000/matt")
+    .then(function (data) {
+      console.log(data);
+      // Create a set to store unique state names
+      const uniqueStateNames = new Set();
+  
+      // Populate the select options with state names
+      const selectElement = d3.select("#selDataset");
+  
+      // Iterate through the state data
+      data.state_data.forEach(state_dataEntry => {
+        const stateName = state_dataEntry.state_name;
+  
+        // Check if the state name is not already in the set
+        if (!uniqueStateNames.has(stateName)) {
+          selectElement.append("option").attr("value", stateName).text(stateName);
+          // Add the state name to the set to mark it as encountered
+          uniqueStateNames.add(stateName);
+        }
+      });
+      
+      // Get the initial state (e.g., "Alabama") for the line chart
+      const initialState = "Alabama";
+      createLineChart(initialState, data.state_data);
+    });
+  
+  // Function to create the line chart
+  function createLineChart(initialState, stateData) {
+    // Filter the state data for the selected state
+    const filteredData = stateData.filter(entry => entry.state_name === initialState);
+  
+    // Group data by bias_desc
+    const groupedData = {};
+  
+    filteredData.forEach(entry => {
+      if (!groupedData[entry.bias_desc]) {
+        groupedData[entry.bias_desc] = {
+          x: [],
+          y: [],
+          mode: 'lines+markers',
+          name: entry.bias_desc,
+        };
+      }
+      groupedData[entry.bias_desc].x.push(entry.data_year);
+      groupedData[entry.bias_desc].y.push(entry.count);
+    });
+  
+    const chartData = Object.values(groupedData);
+  
+    const layout = {
+      title: "Hate Crimes by Bias Over Time",
+      xaxis: { title: "Year" },
+      yaxis: { title: "Count" },
+      height: 600,
+      width: 800,
+    };
+  
+    // Create the line chart using Plotly
+    Plotly.newPlot("line", chartData, layout);
+  }

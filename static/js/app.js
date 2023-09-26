fb0ef302eae9879145db711c51730a11f6fe937e
mattcalvrt <matthew.a.calvert@gmail.com>
2023-09-25 18:21:42 -0700

    update js for state filter

--- static/js/app.js
+++ static/js/app.js
@@ -3,71 +3,77 @@
 d3.json("http://127.0.0.1:5000/data")
   .then(function (data) {
     console.log(data); 
-    })
+  });
 
-  d3.json("http://127.0.0.1:5000/matt")
-    .then(function (data) {
-      console.log(data);
-      // Create a set to store unique state names
-      const uniqueStateNames = new Set();
-  
-      // Populate the select options with state names
-      const selectElement = d3.select("#selDataset");
-  
-      // Iterate through the state data
-      data.state_data.forEach(state_dataEntry => {
-        const stateName = state_dataEntry.state_name;
-  
-        // Check if the state name is not already in the set
-        if (!uniqueStateNames.has(stateName)) {
-          selectElement.append("option").attr("value", stateName).text(stateName);
-          // Add the state name to the set to mark it as encountered
-          uniqueStateNames.add(stateName);
-        }
-      });
-      
-      // Get the initial state (e.g., "Alabama") for the line chart
-      const initialState = "Alabama";
-      createLineChart(initialState, data.state_data);
-    });
-  
-  // Function to create the line chart
-  function createLineChart(initialState, stateData) {
-    // Filter the state data for the selected state
-    const filteredData = stateData.filter(entry => entry.state_name === initialState);
-  
-    // Group data by bias_desc
-    const groupedData = {};
-  
-    filteredData.forEach(entry => {
-      if (!groupedData[entry.bias_desc]) {
-        groupedData[entry.bias_desc] = {
-          x: [],
-          y: [],
-          mode: 'lines+markers',
-          name: entry.bias_desc,
-        };
+d3.json("http://127.0.0.1:5000/matt")
+  .then(function (data) {
+    console.log(data);
+    // Create a set to store unique state names
+    const uniqueStateNames = new Set();
+
+    // Populate the select options with state names
+    const selectElement = d3.select("#selDataset");
+
+    // Iterate through the state data
+    data.state_data.forEach(state_dataEntry => {
+      const stateName = state_dataEntry.state_name;
+
+      // Check if the state name is not already in the set
+      if (!uniqueStateNames.has(stateName)) {
+        selectElement.append("option").attr("value", stateName).text(stateName);
+        // Add the state name to the set to mark it as encountered
+        uniqueStateNames.add(stateName);
       }
-      groupedData[entry.bias_desc].x.push(entry.data_year);
-      groupedData[entry.bias_desc].y.push(entry.count);
+    });
+    
+    // Set up an event listener for the select element change event
+    selectElement.on("change", function() {
+      let selectedState = this.value; // Get the selected state from the dropdown
+      createLineChart(selectedState, data.state_data);
     });
 
-    // Sort data within each bias_desc group by data_year
-    Object.values(groupedData).forEach(group => {
-      const sortedIndices = group.x.map((_, i) => i).sort((a, b) => group.x[a] - group.x[b]);
-      group.x = sortedIndices.map(i => group.x[i]);
-      group.y = sortedIndices.map(i => group.y[i]);
-    });
+    // Get the initial state (e.g., "Alabama") for the line chart
+    let selectedState = "Alabama";
+    createLineChart(selectedState, data.state_data);
+  });
 
-    const chartData = Object.values(groupedData);
-  
-    const layout = {
-      title: "Hate Crimes by Bias Over Time",
-      xaxis: { title: "Year" },
-      yaxis: { title: "Incident Count" },
-      height: 600,
-      width: 1200,
-    };
+// Function to create the line chart
+function createLineChart(selectedState, stateData) {
+  // Filter the state data for the selected state
+  const filteredData = stateData.filter(entry => entry.state_name === selectedState);
+
+  // Group data by bias_desc
+  const groupedData = {};
+
+  filteredData.forEach(entry => {
+    if (!groupedData[entry.bias_desc]) {
+      groupedData[entry.bias_desc] = {
+        x: [],
+        y: [],
+        mode: 'lines+markers',
+        name: entry.bias_desc,
+      };
+    }
+    groupedData[entry.bias_desc].x.push(entry.data_year);
+    groupedData[entry.bias_desc].y.push(entry.count);
+  });
+
+  // Sort data within each bias_desc group by data_year
+  Object.values(groupedData).forEach(group => {
+    const sortedIndices = group.x.map((_, i) => i).sort((a, b) => group.x[a] - group.x[b]);
+    group.x = sortedIndices.map(i => group.x[i]);
+    group.y = sortedIndices.map(i => group.y[i]);
+  });
+
+  const chartData = Object.values(groupedData);
+
+  const layout = {
+    title: "Hate Crimes by Bias Over Time",
+    xaxis: { title: "Year" },
+    yaxis: { title: "Incident Count" },
+    height: 600,
+    width: 1200,
+  };
 
   // Create the line chart using Plotly
   Plotly.newPlot("chart1", chartData, layout);

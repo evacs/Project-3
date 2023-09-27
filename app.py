0031ea0eb505c27d07afdb6ea0ddde791ef34742
zinor3026 <tnordon18@gmail.com>
2023-09-26 18:09:00 -0700

    Updated

--- app.py
+++ app.py
@@ -2,82 +2,27 @@
 # Flask App
 
 from flask import Flask, jsonify, render_template
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, func, extract
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from collections import Counter
-from sqlalchemy import func
-
+import datetime as dt
 
 app = Flask(__name__)
 
-# # LOCAL DB:
-# ---------------------------------------------------------------------------------
-# DB_USERNAME = 'postgres'
-# DB_PASSWORD = 'PASSWORD'
-# DB_HOST = 'localhost'  # e.g., localhost or database server IP
-# DB_PORT = '5432'  # PostgreSQL default port is 5432
-# DB_NAME = 'hatecrimes' # your db name
-# db_uri = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-# --------------------------------------------------------------------------------------
+# Create a SQLAlchemy database engine
+db_url = 'postgresql://postgres:bootcamp2023@localhost:5432/us_hate_crimes'
+# db_url = 'postgresql://admin:fRFTp6MgD7AgfQYMYmyM5jaR8KAfKyXV@dpg-ck56k66ru70s738p5s4g-a.oregon-postgres.render.com:5432/us_hate_crimes'
+engine = create_engine(db_url)
 
-# # ONLINE DB:
-db_uri = 'postgresql://admin:fRFTp6MgD7AgfQYMYmyM5jaR8KAfKyXV@dpg-ck56k66ru70s738p5s4g-a.oregon-postgres.render.com:5432/us_hate_crimes'
-
-
-engine = create_engine(db_uri)
-# reflect an existing database into a new model
+# Reflect an existing database and tables
 Base = automap_base()
-# reflect the tables
 Base.prepare(autoload_with=engine)
 
-#print(Base.classes.keys())
 session = Session(engine)
 
-# ---------------------------------------
-# DEFINE YOUR ROUTES:
-# ---------------------------------------
+print('Connected to database and session initiated')
 
-# @app.route('/general')
-# def get_data():
-#     try:
-#         your code    
-#         return jsonify(data)
-#     except Exception as e:
-#         print("Error accessing the table:", str(e))
-#         return jsonify({"error": "Table access failed"}), 500
-
-# #Define static routes
-# # Variables for related tables
-# bias = Base.classes.bias
-# bias_categories = Base.classes.bias_categories
-# main_inc = Base.classes.main_incidents
-
-# # query statement
-# sel = [main_inc.data_year, main_inc.state_name, main_inc.bias_desc,
-#         main_inc.incident_id, bias_categories.category]
-
-# # join statement
-# query = session.query(*sel)\
-#         .filter(bias.bias == main_inc.bias_desc)\
-#         .filter(bias.category_id == bias_categories.category_id)
-
-# # Query statement
-# result = query.group_by(
-#     main_inc.state_name,
-#     bias_categories.category,
-#     main_inc.data_year).all()
-
-# # Create a list of dictionaries
-# keys = ["year","state","bias","id","category"]
-# bias_dict = []
-# bias_dict = [dict(zip(keys, item)) for item in result]
-# # Assign the metadata list to the "metadata" key in the data dictionary
-# dataToReturn = {"bias_data": bias_dict}
-# print(dataToReturn[:5])
-
-
-
+# Define static routes
 
 # Launches site
 @app.route('/') 
@@ -125,32 +70,29 @@ def get_data_bias():
 # Route to access data for time vs bias per state chart
 @app.route('/matt')
 def get_data_matt():
-
     try:
-        
-        # Save a reference to the main_incidents table as `main_incidents`
-        main_incidents = Base.classes.main_incidents
+        # Variables for related tables
+        bias = Base.classes.bias
+        bias_categories = Base.classes.bias_categories
+        main_inc = Base.classes.main_incidents
 
-        # Design a query that lists the count of hate crimes by state and bias description
-        state_data = session.query(
-            main_incidents.state_name,
-            main_incidents.bias_desc,
-            main_incidents.data_year,
-            func.count(main_incidents.incident_id).label("count")
-        ).group_by(
-            main_incidents.state_name,
-            main_incidents.bias_desc,
-            main_incidents.data_year
-        ).order_by(main_incidents.state_name.asc()).all()
+        # Query statement
+        result = session.query(
+            main_inc.data_year,
+            main_inc.state_name,
+            bias_categories.category,  # Use category instead of bias_desc
+            func.count(main_inc.incident_id).label("count")
+        ).join(bias, bias.bias == main_inc.bias_desc)\
+         .join(bias_categories, bias_categories.category_id == bias.category_id)\
+         .group_by(main_inc.data_year, main_inc.state_name, bias_categories.category)\
+         .order_by(main_inc.data_year.asc()).all()
 
-        # Convert the query result to a list of dictionaries              
-        state_data_list = [{"data_year": row.data_year, "state_name": row.state_name, "bias_desc": row.bias_desc, "count": row.count} for row in state_data]
-        
+        # Create a list of dictionaries
+        keys = ["year", "state", "category", "count"]  # Update keys to include "category"
+        category_dict = [dict(zip(keys, item)) for item in result]
+
         # Assign the metadata list to the "metadata" key in the data dictionary
-        dataToReturn = {"state_data": state_data_list}
-
-        # Print a success message
-        print("Table access successful")
+        dataToReturn = {"state_data": category_dict}
 
         return jsonify(dataToReturn)
 
@@ -158,7 +100,6 @@ def get_data_matt():
         # Handle and log any exceptions
         print("Error accessing the table:", str(e))
         return jsonify({"error": "Table access failed"}), 500
-
 
 
 @app.route('/time')
@@ -181,52 +122,77 @@ def get_data_time():
 
         # Print a success message
         print("Table access successful")
-
+        
+        # return jsonify(data_list)
         return jsonify(dataToReturn)        
-        #return jsonify(data_list)
+        
     except Exception as e:
         print("Error accessing the table:", str(e))
         return jsonify({"error": "Table access failed"}), 500
     
-@app.route('/categories')
-def get_data_categories():
+@app.route('/top10Data')
+def get_top10_data():
 
     try:
-        # Variables for related tables
-        bias = Base.classes.bias
-        bias_categories = Base.classes.bias_categories
-        main_inc = Base.classes.main_incidents
+        # Create dictionary to hold all data
+        dataToReturn = {}
+
+        # Store tables in variables
+        C = Base.classes.census_data
+        S = Base.classes.states
+        I = Base.classes.incidents
+
+        # Create list of all states and add to data dictionary
+        states = []
+        # Don't include Federal Government and Guam in list of states
+        query = session.query(S).filter(S.state_abbr != 'FS').filter(S.state_abbr != 'GM')
+        for row in query.all():
+            states.append(row.state)
+        dataToReturn['states'] = states
+
+        # Query to get population and incident counts by state and year
+        sel = [C.year, S.state, C.population, I.incident_id]
+        query = session.query(C.year, S.state, func.min(C.population).label('population'), func.count(I.incident_id).label('incidents'))
+        query = query.filter(C.state_abbr == S.state_abbr).filter(C.race_id == -1)
+        query = query.filter(I.state_abbr == S.state_abbr).filter(extract('year', I.incident_date) == C.year)
+        query = query.group_by(C.year, S.state)
+        query = query.order_by(func.count(I.incident_id).desc())
+
+        # Create lists for data and years
+        data = []
+        years = []
         
-        # query statement
-        sel = [ main_inc.bias_desc,
-               main_inc.incident_id, bias_categories.category]
+        # Set years in list
+        [years.append(year) for year in range(2009, 2022)]
 
-        # join statement
-        query = session.query(*sel)\
-                .filter(bias.bias == main_inc.bias_desc)\
-                .filter(bias.category_id == bias_categories.category_id)
-
-        # Query statement
-        result = query.all()
-
-        # Create a list of dictionaries
-        keys = ["bias","id","category"]
-        categories_dict = []
-        categories_dict = [dict(zip(keys, item)) for item in result]
-        # Assign the metadata list to the "metadata" key in the data dictionary
-        dataToReturn = {"categories_data": categories_dict}
-                
-        return jsonify(dataToReturn)   
-       
+        # Loop through each year to create data lists
+        for year in years:
+            states = []
+            population = []
+            incidents = []
+            incident_rate = []
+            
+            # Loop through every row for the year
+            for row in query.filter(C.year == year).all():
+                # Append data to list for the year
+                states.append(row.state)
+                population.append(row.population)
+                incidents.append(row.incidents)
+                incident_rate.append(round(row.incidents / row.population * 10000000, 2))
+            
+            # Store lists in a dictionary and append to data list
+            current_year = {'year': year, 'states': states, 'population': population,
+                            'incidents': incidents, 'incident_rate': incident_rate}
+            data.append(current_year)
+        
+        dataToReturn['years'] = years
+        dataToReturn['data'] = data
+        return jsonify(dataToReturn)
 
     except Exception as e:
         # Handle and log any exceptions
         print("Error accessing the table:", str(e))
-        return jsonify({"error": "Table access failed"}), 500
-
-
-
-
+        return jsonify({"error": "Table access failed"}), 500    
 
 
 # THIS GOES AT THE END OF THE FILE ONLY 
--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,206 @@
-// Hate Crimes in the US Code
-
-d3.json("http://127.0.0.1:5000/data")
-  .then(function (data) { 
-    console.log(data); 
-    })
-
-d3.json("http://127.0.0.1:5000/categories")
+d3.json("http://127.0.0.1:5000/bias")
   .then(function(data) {
-    console.log(data);
+    console.log(data);     
   })
   .catch(function (error) {
     console.error("Error loading JSON data:", error);
   });
-  
-  
-  // Extract data for the chart
-    
-    const bias = data.bias;
-    const categories = data.bias_categories;
-    const labels = []  
-    function init() {
-      let data = [{
-        values: categories,
-        labels: categories,
-        type: "pie"
-      }];
-    
-      let layout = {
-        height: 600,
-        width: 800
+
+d3.json("http://127.0.0.1:5000/matt")
+  .then(function (data) {
+    console.log(data);
+
+    // Create a set to store unique state names
+    const uniqueStateNames = new Set();
+
+    // Populate the select options with state names
+    const selectElement = d3.select("#selDataset");
+
+    // Iterate through the state data
+    data.state_data.forEach(state_dataEntry => {
+      const stateName = state_dataEntry.state;
+
+      // Check if the state name is not already in the set
+      if (!uniqueStateNames.has(stateName)) {
+        selectElement.append("option").attr("value", stateName).text(stateName);
+        // Add the state name to the set to mark it as encountered
+        uniqueStateNames.add(stateName);
+      }
+    });
+
+    // Set up an event listener for the select element change event
+    selectElement.on("change", function () {
+      let selectedState = this.value; // Get the selected state from the dropdown
+      createLineChart(selectedState, data.state_data);
+    });
+
+    // Get the initial state (e.g., "Alabama") for the line chart
+    let selectedState = "Alabama";
+    createLineChart(selectedState, data.state_data);
+  });
+
+// Function to create the line chart
+function createLineChart(selectedState, stateData) {
+  // Filter the state data for the selected state
+  const filteredData = stateData.filter(entry => entry.state === selectedState);
+
+  // Group data by bias category
+  const groupedData = {};
+
+  filteredData.forEach(entry => {
+    if (!groupedData[entry.category]) {
+      groupedData[entry.category] = {
+        x: [],
+        y: [],
+        mode: 'lines+markers',
+        name: entry.category,
       };
-    
-      Plotly.newPlot("pie", data, layout);
     }
+    groupedData[entry.category].x.push(entry.year);
+    groupedData[entry.category].y.push(entry.count);
+  });
+
+  // Sort data within each bias group by year
+  Object.values(groupedData).forEach(group => {
+    const sortedIndices = group.x.map((_, i) => i).sort((a, b) => group.x[a] - group.x[b]);
+    group.x = sortedIndices.map(i => group.x[i]);
+    group.y = sortedIndices.map(i => group.y[i]);
+  });
+
+  const chartData = Object.values(groupedData);
+
+  const layout = {
+    title: "Hate Crimes by Bias Category Over Time",
+    xaxis: { title: "Year" },
+    yaxis: { title: "Incident Count" },
+    height: 600,
+    width: 1200,
+  };
+
+  // Create the line chart using Plotly
+  Plotly.newPlot("chart1", chartData, layout);
+}
+
+d3.json("http://127.0.0.1:5000/time")
+  .then(function(data) {
+    console.log(data);
+    time_data = data;
+    timelineChart(time_data);
+  })
+  .catch(function (error) {
+    console.error("Error loading JSON data:", error);
+  });
+
+  function timelineChart(timeData) {
+    // Extract data for the chart
+    const data = timeData.time_data;
+    const years = data.map(item => item.data_year);
+    const counts = data.map(item => item.count);
+    
+    // Create a trace for the line chart
+    const trace = {
+      x: years,
+      y: counts,
+      type: 'line',
+      mode: 'lines+markers', // Show markers on data points
+      marker: {
+        color: 'blue' // Customize line color
+      }
+    };
+  
+    // Specify the layout options for the chart
+    const layout = {
+      title: "Total Hate Crimes Over Time",
+      xaxis: { title: "Year" },
+      yaxis: { title: "Incident Count" },
+    };
+  
+    // Create a data array with the trace
+    const plotData = [trace];
+  
+    // Render the line chart in the specified container
+    Plotly.newPlot('chart2', plotData, layout);
+  }
+  
+d3.json("http://127.0.0.1:5000/top10Data")
+  .then(function (data) { 
+    console.log('Top 10 Data', data);
+    
+    // Store JSON data in separate variables
+    let states = data.states;
+    let years = data.years;
+    let chart_data = data.data;
+    
+    // Log first row of data in variables for review
+    console.log('States:', states);
+    console.log('Years:', years);
+    console.log('First year (2009):', chart_data[0]);
+    
+    const selectYear = d3.select('#selYear');
+    let initial_year = 2021;
+
+    createYearDropDown(years);
+    createTop10Chart(chart_data[initial_year - 2009]);
+    
+    // Set up an event listener for the select element change event
+    selectYear.on("change", function() {
+      let year = this.value; // Get the selected year from the dropdown
+      console.log(year);
+      createTop10Chart(chart_data[year - 2009]);
+    });
+
+  });
+
+function yearChanged(year) {
+  
+  let new_chart_data = chart_data[year - 2009];
+  createTop10Chart(new_chart_data);
+}
+
+function createYearDropDown(years) {
+    
+  // Loop through all names/IDs and add drop down menu option for each test subject
+  for(let i=0; i<years.length; i++) {
+    year = years[i]; 
+    // Add option elementference for updating charts/info 
+    d3.select('#selYear').append('option').text(year).attr('value', year);
+  };
+}
+    
+function createTop10Chart(info) {
+
+  let states = info.states;
+  let incidents = info.incidents;
+  let incident_rate = info.incident_rate;
+  
+  states = states.slice(0, 10).reverse();
+  incidents = incidents.slice(0, 10).reverse();
+  incident_rate = incident_rate.slice(0, 10).reverse();
+
+  // Create data variable for plotting charts
+  let data = [{
+    x: incidents,
+    y: states,
+    text: incident_rate,
+    type: 'bar',
+    orientation: 'h'
+  }];
+    
+  // Log data for review
+  console.log('Initial Bar Chart Data:', data)
+       
+  let layout = {
+    title: 'Top 10 States with the Most Incidents',
+    height: 425,
+    width: 500,
+    margin: {
+      l: 120, // Create gap between demographic info and chart,
+      t: 0, // Align chart with top of subject drop down
+      b: 25 // Reduce gap with bubble chart
+    }
+  };
+    
+  // Plot chart
+  Plotly.newPlot('top10bar', data, layout);
+    
+}
